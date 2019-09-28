5443df0c86dc8bda4652ca1535cc03c65e3b32e9
Dylan <heavilond@gmail.com>
2019-09-28 13:33:21 -0400

    Timer Placeholders Tested each function using debugger = true. Still need to add: Time Functions to decrement HP and put proper Rewards Formula game.py + item.py need to be added together and have seperate classes for each game.

--- checklist.py
+++ checklist.py
@@ -9,6 +9,10 @@ class Creature:
         self.health += add_hp
         self.exp += add_xp
 
+    def time_up(self):
+        lose_hp = -15
+        self.add_hp_xp(lose_hp, 0)
+
 
 c1 = Creature()
 print('Creature HP:' + str(c1.health))
@@ -20,22 +24,24 @@ class Item:
     reward_xp = 0
     reward_hp = 0
 
-    def __init__(self, time_left, what_to_do, item_value):
-        self.time_left = time_left
+    def __init__(self, timer_start, timer_end, what_to_do, item_value):
+        self.timer_start = timer_start
+        self.timer_end = timer_end
         self.what_to_do = what_to_do
         self.item_value = item_value
 
     def reward_amt(self):
-        self.reward_xp = self.time_left * self.item_value / 2
-        if self.time_left / 2 > 25:
+        self.reward_xp = (self.timer_end - self.timer_start) * self.item_value / 2
+        if (self.timer_end - self.timer_start) / 2 > 25:
             self.reward_hp = 25
         else:
-            self.reward_hp = self.time_left / 2
+            self.reward_hp = (self.timer_end - self.timer_start) / 2
 
     def complete_task(self):
         self.item_complete = True
         self.reward_amt()
         c1.add_hp_xp(self.reward_hp, self.reward_xp)
+        c1.hunger_time.cancel()
 
 
 class Checklist:
@@ -58,18 +64,19 @@ class Checklist:
 
 if debugger:
 
-    time_to_do = 30
+    time_start = 60
+    time_end = 30
     item_weight = 2
     test = Checklist()
-    test_item1 = Item(time_to_do, "Fix my Checklist", item_weight)
-    test_item2 = Item(time_to_do, "Create a check off function", item_weight)
-    test_item3 = Item(time_to_do, "Figure out the rest of the details on the list", item_weight)
-    test_item4 = Item(time_to_do, "Apologize to sarah because I used lists over arrays", item_weight)
-    test_item5 = Item(time_to_do, "Finally get sleep after tormenting self past 4am", item_weight)
-    test_item6 = Item(time_to_do, "Try to wake up for breakfast.", item_weight)
-    test_item7 = Item(time_to_do, "Most issues get fixed.", item_weight)
-    test_item8 = Item(time_to_do, "Attempt to test crossout function.", item_weight)
-    test_item9 = Item(time_to_do, "The Bed Calls to me.", item_weight)
+    test_item1 = Item(time_start, time_end, "Fix my Checklist", item_weight)
+    test_item2 = Item(time_start, time_end, "Create a check off function", item_weight)
+    test_item3 = Item(time_start, time_end, "Figure out the rest of the details on the list", item_weight)
+    test_item4 = Item(time_start, time_end, "Apologize to sarah because I used lists over arrays", item_weight)
+    test_item5 = Item(time_start, time_end, "Finally get sleep after tormenting self past 4am", item_weight)
+    test_item6 = Item(time_start, time_end,"Try to wake up for breakfast.", item_weight)
+    test_item7 = Item(time_start, time_end, "Most issues get fixed.", item_weight)
+    test_item8 = Item(time_start, time_end, "Attempt to test crossout function.", item_weight)
+    test_item9 = Item(time_start, time_end, "The Bed Calls to me.", item_weight)
     test.add_list(test_item1)
     test.add_list(test_item2)
     test.add_list(test_item3)
